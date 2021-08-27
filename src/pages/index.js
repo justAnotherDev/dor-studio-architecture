cde2ecc70bb0d7bb698f2e63c7fe6438068faeae
Hunter Evanoff <hunter547@gmail.com>
2021-08-27 15:47:13 -0600

    Updated the favicon to have a white background, so the logo can be seen.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,11 @@
 import * as React from "react"
-import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout/layout"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
   <>
     <Seo title="" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={1000}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
+    <h1>Home</h1>
   </>
 )
 IndexPage.Layout = Layout
